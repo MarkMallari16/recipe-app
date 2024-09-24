f3f684c65435b398302a7b72a24c32fd105a13b4
MarkMallari16 <mallariftw01@gmail.com>
2024-09-25 05:19:42 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1 class="text-2xl">Hello</h1>
+</template>
+
+<style scoped>
+
+</style>
